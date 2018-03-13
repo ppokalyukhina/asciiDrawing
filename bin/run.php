c7a8e03e613034a282c0cd7251aa9d6d8d048a9a
Polina Pokalyukhina <Polina.Pokalyukhina@trivago.com>
2018-03-13 13:42:22 +0100

    added printing random size figures

--- bin/run.php
+++ bin/run.php
@@ -6,57 +6,59 @@ use Plotting\StarsFactory;
 
 require \realpath(__DIR__ . '/../vendor/autoload.php');
 
-// User input (could have been given by get parameters in browser...
-// ... or args in command line)
-$userInput = [null, 25];
 $starsFactory = new StarsFactory();
 $christmasTree = new ChristmasTree();
 
 $smallStarsInstance = StarsFactory::createStarsObject(FigurePlotting::SMALL_SIZE);
+echo "Small size of star \n";
 echo $smallStarsInstance->generate(FigurePlotting::SMALL_SIZE) . "\n";
-echo "See above a small size of star \n";
 
 $mediumStarsInstance = StarsFactory::createStarsObject(FigurePlotting::SMALL_SIZE);
+echo "Middle size of star \n";
 echo $smallStarsInstance->generate(FigurePlotting::MEDIUM_SIZE) . "\n";
-echo "See above a middle size of star \n";
 
 $largeStarsInstance = StarsFactory::createStarsObject(FigurePlotting::LARGE_SIZE);
+echo "Large size of star \n";
 echo $largeStarsInstance->generate(FigurePlotting::LARGE_SIZE) . "\n";
-echo "See above a big size of star \n";
+
+// User input (could have been given by get parameters in browser...
+// ... or args in command line)
+$userInput = [null, 25];
 
 $inputKey = \array_rand($userInput, 1);
-$inputValue = $userInput[$inputKey];
+$inputValue = $userInput[$inputKey]; //Get a random hypothetical user input
 if ($inputValue !== null) {
     try {
         $userInputStarsIntance = StarsFactory::createStarsObject(FigurePlotting::SMALL_SIZE);
         $customizedStar        = $userInputStarsIntance->generate($inputValue);
+        echo "A star generated of {$inputValue} user input lines \n";
         echo "\n" . $customizedStar . "\n";
-        echo "See above a star made of {$inputValue} lines \n";
 
         $customizedTree = $christmasTree->generate($inputValue);
+        echo "Christmas Tree made of {$inputValue} user input lines \n";
         echo "\n" . $customizedTree . "\n";
-        echo "See above a Christmas Tree made of {$inputValue} lines \n";
 
-    }
-    catch (\InvalidArgumentException $exception) {
+    } catch (\InvalidArgumentException $exception) {
         echo "Your input was invalid. Please see details: {$exception->getMessage()} \n";
     }
 } else {
     do {
-        $random = random_int(5, 25); //Could use any max odd number.
-    }
-    while($random % 2 == 0);
+        $random = random_int(5, 25); //Could be used any max odd number.
+    } while ($random % 2 == 0);
 
     $randomStarsInstance = StarsFactory::createStarsObject(FigurePlotting::SMALL_SIZE);
+    echo "Star made of {$random} lines \n";
     echo $randomStarsInstance->generate($random) . "\n";
-    echo "See above a star made of {$random} lines \n";
+
+    echo "Christmas Tree made of {$random} random lines";
+    echo $christmasTree->generate($random) . "\n";
 }
 
+echo "Large size of Christmas Tree \n";
 echo $christmasTree->generate(FigurePlotting::LARGE_SIZE) . "\n";
-echo "See above a large size of Christmas Tree \n";
 
+echo "Medium size of Christmas Tree \n";
 echo $christmasTree->generate(FigurePlotting::MEDIUM_SIZE) . "\n";
-echo "See above a medium size of Christmas Tree \n";
 
+echo "Small size of Christmas Tree \n";
 echo $christmasTree->generate(FigurePlotting::SMALL_SIZE) . "\n";
-echo "See above a small size of Christmas Tree \n";
--- web/index.php
+++ web/index.php
@@ -1,42 +1,41 @@
 <?php
 
-use Plotting\FigurePlottingHelper;
 use Plotting\Figures\ChristmasTree;
 use Plotting\FigurePlotting;
 use Plotting\StarsFactory;
 
 require __DIR__ . '/../vendor/autoload.php';
 
-// User input (could have been given by get parameters in browser...
-// ... or args in command line)
-$userInput = [null, 25];
 $christmasTree = new ChristmasTree();
 
 $smallStarsInstance = StarsFactory::createStarsObject(FigurePlotting::SMALL_SIZE);
+echo "Small size of star.";
 echo "<pre>{$smallStarsInstance->generate(FigurePlotting::SMALL_SIZE)}</pre>";
-echo "See above a small size of star";
 
 $mediumStarsInstance = StarsFactory::createStarsObject(FigurePlotting::SMALL_SIZE);
+echo "Middle size of star.";
 echo "<pre>{$smallStarsInstance->generate(FigurePlotting::MEDIUM_SIZE)}</pre>";
-echo "See above a middle size of star";
 
 $largeStarsInstance = StarsFactory::createStarsObject(FigurePlotting::LARGE_SIZE);
+echo "Large size of star.";
 echo "<pre>{$largeStarsInstance->generate(FigurePlotting::LARGE_SIZE)}</pre>";
-echo "See above a big size of star";
+
+// User input (could have been given by get parameters in browser...
+// ... or args in command line)
+$userInput = [null, 25];
 
 $inputKey = \array_rand($userInput, 1);
-$inputValue = $userInput[$inputKey];
+$inputValue = $userInput[$inputKey]; //Get a random hypothetical user input
 if ($inputValue !== null) {
     try {
         $userInputStarsIntance = StarsFactory::createStarsObject(FigurePlotting::SMALL_SIZE);
         $customizedStar        = $userInputStarsIntance->generate($inputValue);
+        echo "A star generated of {$inputValue} user input lines";
         echo "<pre>{$customizedStar}</pre>";
-        echo "See above a star made of {$inputValue} lines \n";
 
         $customizedTree = $christmasTree->generate($inputValue);
+        echo "Christmas Tree made of {$inputValue} user input lines";
         echo "<pre>{$customizedTree}</pre>";
-        echo "See above a Christmas Tree made of {$inputValue} lines \n";
-
     }
     catch (\InvalidArgumentException $exception) {
         echo "Your input was invalid. Please see details: {$exception->getMessage()}";
@@ -49,16 +48,20 @@ if ($inputValue !== null) {
 
     $randomStarsInstance = StarsFactory::createStarsObject(FigurePlotting::SMALL_SIZE);
     $customizedStar = $randomStarsInstance->generate($random);
+    echo "Star made of {$random} random lines.";
     echo "<pre>{$customizedStar}</pre>";
-    echo "See above a star made of {$random} lines";
+
+    $customizedTree = $christmasTree->generate($random);
+    echo "Christmas Tree made of {$random} random lines";
+    echo "<pre>{$customizedTree}</pre>";
 }
 
+echo "Large size of Christmas Tree.";
 echo "<pre>{$christmasTree->generate(FigurePlotting::LARGE_SIZE)}</pre>";
-echo "See above a large size of Christmas Tree";
 
+echo "Medium size of Christmas Tree.";
 echo "<pre>{$christmasTree->generate(FigurePlotting::MEDIUM_SIZE)}</pre>";
-echo "See above a medium size of Christmas Tree";
 
+echo "Small size of Christmas Tree.";
 echo "<pre>{$christmasTree->generate(FigurePlotting::SMALL_SIZE)}</pre>";
-echo "See above a small size of Christmas Tree";
 
